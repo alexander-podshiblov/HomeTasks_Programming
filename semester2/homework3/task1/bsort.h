72a9f397fc8788dc56b88b96afa8b9f3927ef7c8
Alexander Podshiblov <alexander.podshiblov@gmail.com>
2012-04-06 19:32:59 +0400

    added doxygen comments

--- semester2/homework3/task1/bsort.h
+++ semester2/homework3/task1/bsort.h
@@ -2,8 +2,17 @@
 #include <stdio.h>
 #pragma once
 
+/**
+@class BSort
+класс, сортирующий массив элементов типа int методом пузырьковой сортировки
+*/
 class BSort : public Sort
 {
 public:
+    /**
+    сортировка
+    @param m - указатель на первый элемент массива
+    @param n - количество элементов в массиве
+    */
     void sorting(int *m, int n);
 };
--- semester2/homework3/task1/sort.h
+++ semester2/homework3/task1/sort.h
@@ -1,7 +1,16 @@
 #pragma once
 
+/**
+@class Sort
+интерфейс сортировщика массива элементов типа int
+*/
 class Sort
 {
 public:
+/**
+метод, сортирующий массив
+@param m - указатель на первый элемент массива
+@param n - количество элементов в массиве
+*/
     virtual void sorting(int *m, int n) = 0;
 };
