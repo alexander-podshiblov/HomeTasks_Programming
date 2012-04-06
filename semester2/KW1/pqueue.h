682491256381132606a130dcf82d51a3d4d30adb
Alexander Podshiblov <alexander.podshiblov@gmail.com>
2012-04-06 10:43:02 +0400

    first commit without comments and exception

---
+++ semester2/KW1/pqueue.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#pragma once
+
+template <typename T>
+class PQueue
+{
+public:
+    PQueue()
+    {
+        head = new Node;
+        head->next = NULL;
+    }
+
+    ~PQueue()
+    {
+        while (head->next != NULL)
+        {
+            Node *tmp = head->next;
+            head->next = tmp->next;
+            delete tmp;
+        }
+        delete head;
+    }
+
+    void enqueue(T value, int priority)
+    {
+        Node *tmp = head;
+        while ((tmp->next != NULL) && (priority < tmp->next->priority))
+            tmp = tmp->next;
+        Node *tmp2 = new Node;
+        tmp2->next = tmp->next;
+        tmp2->value = value;
+        tmp2->priority = priority;
+        tmp->next = tmp2;
+    }
+
+    T dequeue()
+    {
+        if (head->next != NULL)
+        {
+            T k;
+            Node *tmp = head->next;
+            head->next = tmp->next;
+            k = tmp->value;
+            delete tmp;
+            return k;
+        }
+    }
+
+private:
+    struct Node
+    {
+        int priority;
+        T value;
+        Node *next;
+    };
+
+    Node *head;
+};
